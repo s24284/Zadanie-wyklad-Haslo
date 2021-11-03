3a7520d46de3cec12453d697b41eb17f2e0d5213
Anna Prymas <s24284@pjwstk.edu.pl>
2021-11-03 18:53:48 +0100

    Dodaj pliki projektów.

---
+++ Zadanie wyklad Haslo/Zadanie wyklad Haslo.cpp
@@ -0,0 +1,35 @@
+﻿// Zadanie wyklad Haslo.cpp 
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string password;
+    std::string password2;
+    std::cout << "Podaj haslo:\n";
+    std::cin >> password;
+
+    do
+
+    {
+        std::cout << "Powtorz haslo:\n";
+        std::cin >> password2;
+
+        if (password2 != password)
+        {
+            std::cout << "Haslo nie jest takie samo! Sprobuj jeszcze raz:\n";
+        }
+
+           
+    } 
+    
+    while (password2 != password);
+
+        std::cout << "Haslo powtorzone poprawnie.";
+
+    return 0;
+
+}
+
